be530b702de2fac345df89628b34c2971097ec27
Kalle-KO <168068721+Kalle-KO@users.noreply.github.com>
2024-10-29 10:14:26 +0100

    Tilføjede IntelliJ-dokument
    projekt laves i dette dokument

---
+++ OnskegrisenProjekt/src/main/java/org/example/onskegrisenprojekt/OnskegrisenProjektApplication.java
@@ -0,0 +1,13 @@
+package org.example.onskegrisenprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskegrisenProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnskegrisenProjektApplication.class, args);
+    }
+
+}
---
+++ OnskegrisenProjekt/src/test/java/org/example/onskegrisenprojekt/OnskegrisenProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onskegrisenprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnskegrisenProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
