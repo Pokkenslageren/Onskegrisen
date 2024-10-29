60718b51a149b9e3d3cbf57a4a4332a86cb8c9ca
Kalle-KO <kaot0001@stud.kea.dk>
2024-10-29 10:39:44 +0100

    //hej

--- OnskegrisenProjekt/src/main/java/org/example/onskegrisenprojekt/OnskegrisenProjektApplication.java
+++ OnskegrisenProjekt/src/main/java/org/example/onskegrisenprojekt/OnskegrisenProjektApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//hej
 public class OnskegrisenProjektApplication {
 
     public static void main(String[] args) {
