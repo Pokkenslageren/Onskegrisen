52e41a12cc5cece429d36522afc3511c9117c185
Kalle-KO <kaot0001@stud.kea.dk>
2024-11-06 13:41:33 +0100

    SQL - login deleted Login class made LoginRepo class made

--- OnskegrisenProjekt/src/main/java/OnskeGrisen/Model/Login.java
+++ OnskegrisenProjekt/src/main/java/OnskeGrisen/Model/Login.java
@@ -1,4 +1,24 @@
 package OnskeGrisen.Model;
 
 public class Login {
+    private String loginUserName;
+    private String loginUserPassword;
+
+    public Login(String loginUserName, String loginUserPassword) {
+        this.loginUserName = loginUserName;
+        this.loginUserPassword = loginUserPassword;
+    }
+
+    public String getLoginUserName() {
+        return loginUserName;
+    }
+    public void setLoginUserName(String loginUserName) {
+        this.loginUserName = loginUserName;
+    }
+    public String getLoginUserPassword() {
+        return loginUserPassword;
+    }
+    public void setLoginUserPassword(String loginUserPassword) {
+        this.loginUserPassword = loginUserPassword;
+    }
 }
--- OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/LoginRepository.java
+++ OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/LoginRepository.java
@@ -1,5 +1,45 @@
 package OnskeGrisen.Repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class LoginRepository {
 
+    String database = "jdbc:mysql://localhost:3306/onskegrisen";
+    String dbUsername = "root";
+    String dbPassword = "";
+    Connection conn;
+
+    public boolean login(String loginUserName, String loginPassword) {
+        String query = "SELECT * FROM user WHERE user_name = ? AND user_password = ?";
+        boolean isUser = false;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(database, dbUsername, dbPassword);
+            if (conn == null) {
+                System.out.println("Connection not established");
+            }
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, loginUserName);
+            pstmt.setString(2, loginPassword);
+            ResultSet rs = pstmt.executeQuery();
+            ArrayList<String> credentials = new ArrayList<>();
+            while (rs.next()) {
+                credentials.add(rs.getString("user_name"));
+                credentials.add(rs.getString("user_password"));
+            }
+            if (!credentials.isEmpty()) {
+                isUser = true;
+            }
+            return isUser; // returnerer dobbelt? Skal den ind i if-statement?
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return isUser;
+    }
 }
