c3ece18072a3f599bb41556a119ffc3865f844f6
MagnusHannibal <magnus7f@hotmail.com>
2024-11-06 15:22:50 +0100

    e.printStackTrace() added to all CRUD functions instead of e.getMessage(). Fixes for every DELETE function in repos. Fixes for READ in wish repo

--- OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/WishRepository.java
+++ OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/WishRepository.java
@@ -38,34 +38,38 @@ public class WishRepository {
             pstmt.close();
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public Wish readWishByTitle(String wishTitle){
-        String query = "SELECT * WHERE wish_title = ?";
+    public Wish readWishByTitle(String wishTitle, String wishListName){
+        String query = "SELECT * FROM wish WHERE wish_title = ? AND wishlist_name =?";
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(database,dbUsername,dbPassword);
             if (conn == null) {
                 System.out.println("Connection not established");
+                return null;
             }
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setString(1,wishTitle);
+            pstmt.setString(2,wishListName);
             ResultSet rs = pstmt.executeQuery();
-            while(rs.next()) {
+
+            if (rs.next()) {
                 String readWishListOwner = rs.getString("wishlist_owner");
                 String readWishListName = rs.getString("wishlist_name");
-                String readWishTitle = rs.getString("wishlist_title");
-                String readWishDescription = rs.getString("wishlist_description");
+                String readWishTitle = rs.getString("wish_title");
+                String readWishDescription = rs.getString("wish_description");
                 double readWishPrice = rs.getDouble("wish_price");
                 String readWishLink = rs.getString("wish_link");
                 boolean isReserved = rs.getBoolean("is_reserved");
+
                 return new Wish(readWishListOwner, readWishListName, readWishTitle, readWishDescription, readWishPrice, readWishLink, isReserved);
             }
         }
         catch (Exception e){
-            e.getMessage();
+            e.printStackTrace();
         }
         return null;
     }
@@ -91,12 +95,12 @@ public class WishRepository {
             pstmt.close();
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public void deleteWish(Wish wish, String wishTitle){
-        String query = "DELETE FROM wish WHERE wish_title = ?";
+    public void deleteWish(Wish wish){
+        String query = "DELETE FROM wish WHERE wish_title = ? AND wishlist_name = ?";
 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");;
@@ -105,12 +109,13 @@ public class WishRepository {
                 System.out.println("Connection not established");
             }
             PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setString(1,wishTitle);
+            pstmt.setString(1,wish.getWishTitle());
+            pstmt.setString(2,wish.getWishListName());
             pstmt.executeUpdate();
             pstmt.close();
 
         }catch (Exception e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
