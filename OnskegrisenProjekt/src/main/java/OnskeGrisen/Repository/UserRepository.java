9fe4369fc26e2c9f7765ef56fdf03d616cf25f32
MagnusHannibal <magnus7f@hotmail.com>
2024-11-13 14:07:56 +0100

    delete now works correctly in user-page.html

--- OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/UserRepository.java
+++ OnskegrisenProjekt/src/main/java/OnskeGrisen/Repository/UserRepository.java
@@ -10,7 +10,7 @@ public class UserRepository {
     ArrayList<User> users = new ArrayList<>();
     String database = "jdbc:mysql://localhost:3306/onskegrisen";
     String dbUsername = "root";
-    String dbPassword = "Illcosby91";
+    String dbPassword = "root";
     Connection conn;
 
 
